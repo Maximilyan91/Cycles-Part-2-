09af5cf50d2a54b773958169e834de67504a37cc
Максим <gmax7911@gmail.com>
2022-12-13 08:41:26 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задача 1.
 
+        int monthPay = 15000;
+        int totalPay = 0;
+        int month = 1;
+        while (totalPay <= 2459000) {
+            month += 1;
+            totalPay = totalPay + totalPay/100;
+            totalPay = totalPay + monthPay;
+            System.out.println("Месяц " + month + " Сумма накоплений = " + totalPay);
+        }
 
 
 
