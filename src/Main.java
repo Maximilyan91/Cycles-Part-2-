73896476417a8dda206407126df05b5eb2d3da03
Максим <gmax7911@gmail.com>
2022-12-13 08:51:34 +0300

    Задача 2 Выполнена.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,21 @@ public class Main {
             System.out.println("Месяц " + month + " Сумма накоплений = " + totalPay);
         }
 
+        // Задача 2.
+
+        int a = 0;
+        while (a < 10) {
+            a += 1;
+            System.out.print(" " + a);
+        }
+        System.out.println();
+
+        for (a = 10; a > 0; a--) {
+            System.out.print(" " + a);
+        }
+
+
+
 
 
 
